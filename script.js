b5ad32b1571c150e982d7b39e677a4ff31ea865c
taras240 <khomyn@outlook.com>
2023-06-05 15:32:42 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+let digits = document.querySelectorAll(".button__number");
+let enteredNumber = document.querySelector(".enteredNumber");
+let result = document.querySelector(".result");
+let baseNumber = 0;
+let baseString = "";
+let clearNumber = true;
+let timeOut;
+function enterDigit(digit) {
+  if (clearNumber) {
+    enteredNumber.innerText = digit;
+  } else {
+    enteredNumber.innerText += digit;
+  }
+  baseString = enteredNumber.innerText;
+  baseNumber = parseFloat(baseString);
+  result.innerText = (baseNumber * 1.2).toFixed(2);
+  clearNumber = false;
+  clearTimeout(timeOut);
+  timeOut = setTimeout(() => clearDisplay(), 1500);
+}
+function clearDisplay() {
+  enteredNumber.innerText = "";
+}
