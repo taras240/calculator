60bf193a6d48b85c6cb1ce44a2c5c01f71a7adf6
taras240 <khomyn@outlook.com>
2023-06-05 17:07:50 +0300

    vibro

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ let baseString = "";
 let clearNumber = true;
 let timeOut;
 function enterDigit(digit) {
+  window.navigator.vibrate(100);
   if (enteredNumber.classList.contains("inactive")) {
     enteredNumber.classList.remove("inactive");
     enteredNumber.innerText = digit;
