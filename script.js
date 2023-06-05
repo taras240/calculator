fa0113eef4faff1c5464dbd4c83af1f90b1429d3
taras240 <khomyn@outlook.com>
2023-06-05 20:09:48 +0300

    update

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let digits = document.querySelectorAll(".button__number");
 let enteredNumber = document.querySelector(".enteredNumber");
 let result = document.querySelector(".result");
+let quantityElement = document.querySelector(".quantity-number");
+let quantity = 1;
 let baseNumber = 0;
 let baseString = "";
 let percentage = document
@@ -8,8 +10,22 @@ let percentage = document
   .getAttribute("percent");
 let clearNumber = true;
 let timeOut;
+let activeInput = "baseNumber"; //or quantity
 function enterDigit(digit) {
   window.navigator.vibrate(40);
+  switch (activeInput) {
+    case "baseNumber":
+      changeBaseNumber(digit);
+      break;
+    case "quantity":
+      changeQuantity(digit);
+      break;
+    default:
+      break;
+  }
+}
+
+function changeBaseNumber(digit) {
   if (enteredNumber.classList.contains("inactive")) {
     enteredNumber.classList.remove("inactive");
     enteredNumber.innerText = digit;
@@ -21,13 +37,24 @@ function enterDigit(digit) {
   }
   baseString = enteredNumber.innerText;
   baseNumber = parseFloat(baseString);
-  result.innerText = (baseNumber * percentage).toFixed(2);
+  calcPrice();
   clearNumber = false;
   clearTimeout(timeOut);
   timeOut = setTimeout(() => clearDisplay(), 1000);
 }
+function changeQuantity(digit) {
+  quantityElement.innerText += digit;
+  quantity = parseFloat(quantityElement.innerText);
+  calcPrice();
+}
 function clearDisplay() {
   enteredNumber.classList.add("inactive");
+}
+function refreshDisplay() {
+  enteredNumber.innerText = "";
+  result.innerText = "";
+  enteredNumber.classList.remove("inactive");
+  quantityElement.innerText = quantity = 1;
 }
 function changePercenage(element) {
   document.querySelectorAll(".percentage-element").forEach((element) => {
@@ -36,4 +63,16 @@ function changePercenage(element) {
   element.classList.add("active");
   percentage = element.getAttribute("percent");
   result.innerText = (baseNumber * percentage).toFixed(2);
+}
+function setQuantity() {
+  activeInput = "quantity";
+  quantityElement.classList.add("active");
+  quantityElement.innerText = "";
+}
+function setNumber() {
+  activeInput = "baseNumber";
+  quantityElement.classList.remove("active");
+}
+function calcPrice() {
+  result.innerText = ((baseNumber * percentage) / quantity).toFixed(2);
 }
