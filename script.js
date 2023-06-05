c6c38eed631f2ef3131d7c65075949db4c8a2c4c
taras240 <khomyn@outlook.com>
2023-06-05 18:39:54 +0300

    percent added

--- script.js
+++ script.js
@@ -3,10 +3,13 @@ let enteredNumber = document.querySelector(".enteredNumber");
 let result = document.querySelector(".result");
 let baseNumber = 0;
 let baseString = "";
+let percentage = document
+  .querySelector(".percentage-element.active")
+  .getAttribute("percent");
 let clearNumber = true;
 let timeOut;
 function enterDigit(digit) {
-  window.navigator.vibrate(20);
+  window.navigator.vibrate(40);
   if (enteredNumber.classList.contains("inactive")) {
     enteredNumber.classList.remove("inactive");
     enteredNumber.innerText = digit;
@@ -18,12 +21,19 @@ function enterDigit(digit) {
   }
   baseString = enteredNumber.innerText;
   baseNumber = parseFloat(baseString);
-  result.innerText = (baseNumber * 1.2).toFixed(2);
+  result.innerText = (baseNumber * percentage).toFixed(2);
   clearNumber = false;
   clearTimeout(timeOut);
   timeOut = setTimeout(() => clearDisplay(), 1500);
 }
 function clearDisplay() {
   enteredNumber.classList.add("inactive");
-  //   enteredNumber.innerText = "";
+}
+function changePercenage(element) {
+  document.querySelectorAll(".percentage-element").forEach((element) => {
+    element.classList.remove("active");
+  });
+  element.classList.add("active");
+  percentage = element.getAttribute("percent");
+  result.innerText = (baseNumber * percentage).toFixed(2);
 }
